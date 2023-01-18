56061eb61e345afd067bd8068dcc16136afff76a
XandZap <alexandreroberto.94@gmail.com>
2023-01-18 16:20:29 -0300

    ✨ init setup layout

--- front/web/src/App.tsx
+++ front/web/src/App.tsx
@@ -1,16 +1,16 @@
-import Habit from "./components/Habit";
 import "./styles/global.css";
+import Header from "./components/Header";
+import SummaryTable from "./components/SummaryTable";
 
 function App() {
   return (
-    <>
-      <h1>Hello Word</h1>
-      <Habit />
-      <Habit />
-      <Habit />
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   );
 }
 
 export default App;
-
