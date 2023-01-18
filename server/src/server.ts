67204349ab4b9f88ccc5539b5845c766a1f0312a
XandZap <alexandreroberto.94@gmail.com>
2023-01-18 12:39:35 -0300

    ✨ create tables and start routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/", () => {
-  const habits = prisma.habit.findMany();
-  return habits;
-});
+app.register(appRoutes);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running");
